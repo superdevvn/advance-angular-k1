5d817d555cb59787ea0016f9276783f096ada830
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-04-11 19:26:04 +0700

    - Update grid control

---
+++ source demo/grid-control-03/src/app/controls/grid-control/grid-control.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { IGridOption, GridOption } from './grid-control.model';
+import { Http } from '@angular/http';
+
+@Component({
+  selector: 'app-grid-control',
+  templateUrl: './grid-control.component.html',
+  styleUrls: ['./grid-control.component.css']
+})
+export class GridControlComponent implements OnInit {
+  @Input() option: IGridOption;
+  gridOption: GridOption;
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+    this.gridOption = new GridOption();
+    this.gridOption.url = this.option.url;
+    this.gridOption.data = this.option.data;
+    this.gridOption.commands = this.option.commands;
+    this.gridOption.columns = this.option.columns;
+  }
+
+  reload() {
+    let data = {
+      pageSize: this.gridOption.pageSize,
+      pageNumber: this.gridOption.pageNumber,
+      orderBy: this.gridOption.orderBy,
+      orderDirection: this.gridOption.orderDirection
+    };
+    this.http.post(this.gridOption.url, data).toPromise()
+      .then(res => {
+        this.gridOption.total = res.json().TotalItemCount;
+        this.gridOption.data = res.json().Entities;
+        this.gridOption.pageCount = res.json().PageCount;
+      });
+  }
+}
