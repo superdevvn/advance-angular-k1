cf941046ff7f9ce95cfbe2eba89c400ec1587b94
Trần Thuận Nghĩa <ghost.of.dark.peter@gmail.com>
2018-04-22 15:51:01 +0700

    - Update Socket

--- source demo/chat-client/src/app/demo/demo-socket/demo-socket.component.ts
+++ source demo/chat-client/src/app/demo/demo-socket/demo-socket.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { SocketService, SocketData } from '../../services/socket.service';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-demo-socket',
@@ -9,17 +10,32 @@ import { SocketService, SocketData } from '../../services/socket.service';
 export class DemoSocketComponent implements OnInit {
   username: string;
   message: string;
-  constructor(private socketService: SocketService) { 
-    this.socketService.emitter.subscribe((socketData: SocketData)=>{
-      if(socketData.code === "CHAT"){
-        console.log(`${socketData.data.username}: ${socketData.data.message}`);
+  messages: any[] = [];
+  @ViewChild('boxChatRef') boxChat: ElementRef;
+  constructor(private socketService: SocketService) {
+    this.socketService.emitter.subscribe((socketData: SocketData) => {
+      if (socketData.code === "CHAT") {
+        this.messages.push(socketData.data);
+        setTimeout(() => {
+          $(this.boxChat.nativeElement).scrollTop($(this.boxChat.nativeElement).prop("scrollHeight"));
+        }, 1000);        
+        // $(this.boxChat.nativeElement).append(`<span class="user">${socketData.data.username}</span>: <span class="message">${socketData.data.message}</span> <br />`);
+        // console.log(`${socketData.data.username}: ${socketData.data.message}`);
       }
     });
   }
 
   ngOnInit() {
   }
-  send(){
+  send() {
+    this.messages.push({
+      username: this.username,
+      message: this.message
+    });
+    setTimeout(() => {
+      $(this.boxChat.nativeElement).scrollTop($(this.boxChat.nativeElement).prop("scrollHeight"));
+    }, 1000); 
+    //$(this.boxChat.nativeElement).append(`<span class="user">${this.username}</span>: <span class="message">${this.message}</span> <br />`);
     this.socketService.send({
       code: "CHAT",
       data: {
